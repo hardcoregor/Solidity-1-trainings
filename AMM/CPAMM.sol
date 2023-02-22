804b23215d6fdde1e8b32c58f0cee3a49a4c9cec
hardcoregor <spraytown2014@gmail.com>
2023-02-22 11:11:58 +0200

    done AMM

--- AMM/CPAMM.sol
+++ AMM/CPAMM.sol
@@ -28,8 +28,89 @@ contract CPAMM {
         totalSupply -= _amount;
     }
 
+    function _update(uint _reserve0, uint _reserve1) private {
+        reserve0 = _reserve0;
+        reserve1 = _reserve1;
+    }
+
     function swap(address _tokenIn, uint _amount) external returns(uint amountOut) {
-      
+        require(_tokenIn == address(token0) || _tokenIn == address(token1), "Invalid token");
+        require(_amount > 0, "amount in) = 0");
+
+        bool isToken0 = _tokenIn == address(token0);
+        (IERC20 tokenIn, uint reserveIn, IERC20 tokenOut, uint reserverOut) = isToken0
+            ? (token0, token1, reserve0, reserve1)
+            : (token1, token0, reserve1, reserve0);
+
+        tokenIn.tranfserFrom(msg.sender, address(this), _amount);
+
+        uint amountInWithFee = (_amountIn * 997) / 1000;
+        amountOut = (reserverOut * amountInWithFee) / (reserveIn + amountInWithFee);
+
+        _update(token0.balanceOf(address(this)), token1.balanceOf(address(this)));
+
+        tokenOut.transfer(msg.sender, amountOut);
     } 
+
+    function addLiquidity(uint _amount0, uint _amount1) external returns(uint shares) {
+        token0.tranfserFrom(msg.sender, address(this), _amount0);
+        token1.tranfserFrom(msg.sender, address(this), _amount1);
+
+        if(reserve0 > 0 || reserver1 > 0) {
+            require(reserve0 * _amount1 == reserve1 * _amount0, "dy / dy != y / x");
+        }
+
+        if(totalSupply == 0) {
+            shares = _sqrt(_amount0 * _amount1);
+        } else {
+            shares = _min(
+                (_amount0 * totalSupply) / reserve0,
+                (_amount1 * totalSupply) / reserve1
+            );
+        }
+        require(shares > 0, "shares = 0");
+        _mint(msg.sender, shares);
+
+        _update(
+            token0.balanceOf(address(this)),
+            token1.balanceOf(address(this))
+        );
+    }
+
+    function removeLiquidity(uint _shares) external returns(uint amount0, uint amount1) {
+        uint bal0 = token0.balanceOf(address(this));
+        uint bal1 = token1.balanceOf(address(this));
+
+        amount0 = (_shares * bal0) / totalSupply;
+        amount1 = (_shares * bal1) / totalSupply;
+        require(amount0 > 0 && amount1 > 0, "amount0 or amount1 = 0");
+
+        _burn(msg.sender, _shares);
+
+        _update(
+            bal0 - amount0,
+            bal1 - amount1
+        );
+
+        token0.transfer(msg.sender, amount0);
+        token1.transfer(msg.sender, amount1);
+    }
+
+    function _sqrt(uint y) private pure returns (uint z) {
+        if(y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z ) {
+                z = x;
+                x = (y / x + x) / 2;
+            } else if (y != 0 ) {
+                z = 1;
+            }
+        }
+    }
+
+    function _min(uint x, uint y) private pure returns(uint){
+        return x <= y ? x : y;
+    }
     
 }
