8e34cc6762fcebfd1361bd2ed88c5effcc7ce0f6
hardcoregor <spraytown2014@gmail.com>
2023-03-01 11:03:14 +0200

    testing, deploying etc...

---
+++ BigCourse32Hours/after10hours/helper-hardhat-config.js
@@ -0,0 +1,25 @@
+const networkConfig = {
+    5: {
+        name: "goerli",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    },
+    137: {
+        name: "polygon",
+        ethUsdPriceFeed: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
+    },
+    // 31337: {
+    //   name: "localhost",
+    //   ethUsdPriceFeed: "0x0715A7794a1dc8e42615F059dD6e406A6594651A",
+    // }
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const DECIMALS = 8
+const INITIAL_ANSWER = 160000000000
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER,
+}
